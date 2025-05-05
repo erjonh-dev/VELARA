c7b6c0b5519ed87fe78fa27c5ff81ca137020847
Erjon Havolli <Havollierjon@hotmail.com>
2025-05-05 20:23:13 +0200

    update

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -12,7 +12,7 @@ export default function Login() {
     e.preventDefault();
 
     try {
-      const response = await fetch('http://localhost:5000/api/login', {
+      const response = await fetch(`${import.meta.env.VITE_API_URL}/api/register`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ usernameOrEmail: email, password }),
